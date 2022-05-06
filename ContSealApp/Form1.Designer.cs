e7f0563c69e8e3be75e3975acf09e688c819792d
PS <sinatel@inbox.ru>
2022-05-06 15:41:46 +1000

    v.0.1

--- ContSealApp/Form1.Designer.cs
+++ ContSealApp/Form1.Designer.cs
@@ -30,35 +30,53 @@
         {
             this.inputTextBox = new System.Windows.Forms.TextBox();
             this.startButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // inputTextBox
             // 
-            this.inputTextBox.Location = new System.Drawing.Point(39, 12);
+            this.inputTextBox.Location = new System.Drawing.Point(13, 13);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.inputTextBox.Multiline = true;
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(264, 364);
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.inputTextBox.Size = new System.Drawing.Size(338, 447);
             this.inputTextBox.TabIndex = 1;
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(39, 399);
+            this.startButton.Location = new System.Drawing.Point(13, 489);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(264, 48);
+            this.startButton.Size = new System.Drawing.Size(339, 45);
             this.startButton.TabIndex = 2;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(377, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 447);
+            this.textBox1.TabIndex = 3;
+            // 
             // inputForm1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1123, 521);
+            this.ClientSize = new System.Drawing.Size(731, 550);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.inputTextBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "inputForm1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ContTextApp";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -68,5 +86,6 @@
         #endregion
         private TextBox inputTextBox;
         private Button startButton;
+        private TextBox textBox1;
     }
 }
